e280fe9c20023e9d6dab0df0bfd697ae43f7bc1d
Brandon <31749493+BCRevels@users.noreply.github.com>
2017-11-18 03:55:45 -0600

    Game Files

---
+++ Assets/Menu_Buttons.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Menu_Buttons : MonoBehaviour {
+    public GameObject MenuPanel;
+    public GameObject SecondPanel;
+
+    void Start () {
+        MenuPanel.SetActive(true);
+        SecondPanel.SetActive(false);
+    }
+	
+	void Update () {
+		
+	}
+    public void ShowMenuPanel()
+    {
+        MenuPanel.SetActive(true);
+        SecondPanel.SetActive(false);
+    }
+    public void ShowSecondPanel()
+    {
+        MenuPanel.SetActive(false);
+        SecondPanel.SetActive(true);
+    }
+}
+
